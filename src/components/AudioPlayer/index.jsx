ecdcf6198b5415f817fa2cd2d8af1329746dcdde
rinotoharto <rinotoharto@gmail.com>
2022-09-05 03:26:24 +0700

    Fix voice recog & Add BGM

--- src/components/AudioPlayer/index.jsx
+++ src/components/AudioPlayer/index.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState} from 'react';
-import Song from '../../static/music/audio.mp3';
-import Play from '../../static/icons/play.png';
-import Pause from '../../static/icons/pause.png';
+import Song from '../../static/music/bgm.mp3';
 import classes from './style.module.scss';
 
 const AudioPlayer = ({ isPlaying, setIsPlaying}) => {
   const [audio] = useState(new Audio(Song));
-
+  const Pause = 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662315911/Invitation%20Assets/Mela%27s/pause_vhfmki.webp';
+  const Play = 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662315911/Invitation%20Assets/Mela%27s/play_vhdg4x.webp';
   useEffect(() => {
     isPlaying ? audio.play() : audio.pause();
   }, [isPlaying]);
@@ -17,7 +16,7 @@ const AudioPlayer = ({ isPlaying, setIsPlaying}) => {
 
   return (
     <div className={classes.audioContainer}>
-      <img onClick={onClickBtn} src={isPlaying ? Pause : Play} alt="audio-player" />
+      <img className={classes.icon} onClick={onClickBtn} src={isPlaying ? Pause : Play} alt="audio-player" />
     </div>
   );
 };
