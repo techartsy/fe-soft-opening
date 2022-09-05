473fafdd63a9e0570e207de7848466874841780d
rinotoharto <rinotoharto@gmail.com>
2022-09-05 23:40:01 +0700

    fix revition

--- src/static/images/gallery/gallery.js
+++ src/static/images/gallery/gallery.js
@@ -12,10 +12,22 @@ const gallery = [
     img: 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662245634/Invitation%20Assets/Mela%27s/4_wtshsf.webp'
   },
   {
+    img: 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662394800/Invitation%20Assets/Mela%27s/Pigeonwings-Sparkle_kzwu1s.webp'
+  },
+  {
     img: 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662245635/Invitation%20Assets/Mela%27s/5_wzjnjm.webp'
   },
   {
+    img: 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662394800/Invitation%20Assets/Mela%27s/Frozen-Espresso_fds1v4.webp'
+  },
+  {
+    img: 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662394800/Invitation%20Assets/Mela%27s/NZ6_7517_ka1ysd.webp'
+  },
+  {
     img: 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662245634/Invitation%20Assets/Mela%27s/6_jixhka.webp'
+  },
+  {
+    img: 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662394800/Invitation%20Assets/Mela%27s/NZ6_7651_a5efub.webp'
   },
   {
     img: 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662245634/Invitation%20Assets/Mela%27s/7_wjktlz.webp'
