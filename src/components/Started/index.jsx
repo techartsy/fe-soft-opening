058a3032670468568bb791674d19017c725f9d90
rinotoharto <rinotoharto@gmail.com>
2022-09-06 11:43:04 +0700

    fix started page & add meta tag for description

--- src/components/Started/index.jsx
+++ src/components/Started/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import classes from './style.module.scss';
 
-const banner = 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662394216/Invitation%20Assets/Mela%27s/banner_niin8s.webp';
 const btn = 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662235564/Invitation%20Assets/Mela%27s/btn_ihpama.webp';
+const logo = 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662437375/Invitation%20Assets/Mela%27s/logo_yzyogg.webp';
+const info = 'https://res.cloudinary.com/dwvzfit8v/image/upload/v1662437201/Invitation%20Assets/Mela%27s/date_iulncx.webp';
 
 const Started = ({ openInvitation, name }) => {
   return (
     <div className={classes.container}>
       <div className={classes.wraper}>
-        <img className={classes.banner} src={banner} alt="frame" />
+        <img className={classes.logo} src={logo} alt="Melas Dining" />
+        <div className={classes.textWrapper}>
+          {name && <p className={classes.name}>Dear<br/>{name}</p>}
+          <p className={classes.invitingText}>You're Invited To Our</p>
+        </div>
+        <img className={classes.banner} src={info} alt="frame" />
         <p className={classes.instruction}>Click Button Below</p>
         <img className={classes.btn} src={btn} onClick={openInvitation} alt='btn' />
       </div>
